0384fdc1c7159f0a170a0b9cca2cd31c5f8d6bb4
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-08 08:13:30 +0800

    exercise 1-4

--- chapter01/temp.c
+++ chapter01/temp.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
 int main() {
-	float fahr, celsius;
+	float celsius, fahr;
 	int lower, upper, step;
 
-	lower = 0;
-	upper = 300;
-	step = 20;
+	lower = -20;
+	upper = 150;
+	step = 10;
 
-	fahr = lower;
+	celsius = lower;
 
-	printf("F      C\n");
+	printf("C      F\n");
 	printf("----------\n");
 
-	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+	while (celsius <= upper) {
+		fahr = celsius * (9.0 / 5.0) + 32.0;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius += step;
 	}
 }
-
-
