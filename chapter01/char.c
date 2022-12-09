96a7f0cee552c84bb1fcadb1e9fe1d5f4a8ac3fe
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-09 08:29:26 +0800

    exercise 1-10

--- chapter01/char.c
+++ chapter01/char.c
@@ -4,13 +4,15 @@ int main() {
 	int c;
 
 	while ((c = getchar()) != EOF) {
-		if (c == ' ') {
-			while ((c = getchar()) == ' ') {}
-			printf(" ");
+		if (c == '\t') {
+			printf("\f");
+		} else if (c == '\b') {
+			printf("\b");
+		} else if (c == '\\') {
+			printf("\\");
+		} else {
+			printf("%d" ,c);
 		}
-		printf("%c", c);
 	}
 }
 
-/*Exercise 1-9. Write a program to copy its input to its output, replacing each string of one or*/
-/*more blanks by a single blank.*/
