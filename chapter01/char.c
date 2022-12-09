c43f9d36764deb22d86144305ee2a4be65c84624
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-09 08:36:17 +0800

    exercise 1-12

--- chapter01/char.c
+++ chapter01/char.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main() {
-	int c;
+	int c, state;
+
+	state = OUT;
 
 	while ((c = getchar()) != EOF) {
-		if (c == '\t') {
-			printf("\f");
-		} else if (c == '\b') {
-			printf("\b");
-		} else if (c == '\\') {
-			printf("\\");
-		} else {
-			printf("%d" ,c);
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+			printf("\n");
+		} else if (state == OUT) {
+			state = IN;
+			printf("%d", c);
 		}
 	}
 }
