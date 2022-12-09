a64b9024ce2ce0e0910462200c25cdce1a43ffc8
xavier-kong <kongxianchengxavier@gmail.com>
2022-12-09 08:27:06 +0800

    exercise 1-9

--- chapter01/char.c
+++ chapter01/char.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	int c, blank, tabs, nl;
+	int c;
 
 	while ((c = getchar()) != EOF) {
-		if (c == '\n') {
-			++nl;
-		} else if (c == '\t') {
-			++tabs;
-		} else if (c == ' ') {
-			++blank;
+		if (c == ' ') {
+			while ((c = getchar()) == ' ') {}
+			printf(" ");
 		}
+		printf("%c", c);
 	}
-
-	printf("blanks: %d, tabs: %d, new lines: %d", blank, tabs, nl);
 }
 
 /*Exercise 1-9. Write a program to copy its input to its output, replacing each string of one or*/
